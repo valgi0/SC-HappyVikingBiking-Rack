730648448dc08725629fbeefdfb3b8a283e8b801
Tirocigno <fede.naldini1@gmail.com>
2019-05-08 18:34:48 +0200

    Demo is terminated if everything is ok

--- src/main/scala/it/unibo/sc1819/test/client/MockSensorVerticle.scala
+++ src/main/scala/it/unibo/sc1819/test/client/MockSensorVerticle.scala
@@ -1,17 +1,15 @@
 package it.unibo.sc1819.test.client
 
-import io.vertx.core.AbstractVerticle
 import io.vertx.lang.scala.ScalaVerticle
-import io.vertx.scala.core.Vertx
 
 class MockSensorVerticle extends ScalaVerticle {
 
   override def start(): Unit = {
-    val vertx = Vertx.vertx()
+    val vertx = MockClientMain.vertx
     val eventBus = vertx.eventBus()
 
     vertx.setPeriodic(1000, _ => {
-      eventBus.publish("healthcare", "Yay! Someone got lumbago! Notify the server!")
+      eventBus.publish(MockClientMain.TOPIC_ADDRESS, "Yay! Someone got lumbago! Notify the server!")
     })
   }
 
