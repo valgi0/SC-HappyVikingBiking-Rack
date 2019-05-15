e18b5a4e00a67423fa55f026c9529c5a5573027c
Tirocigno <fede.naldini1@gmail.com>
2019-05-15 11:29:53 +0200

    Modified for working without raspberry

--- src/main/scala/it/unibo/sc1819/main/MockScalaMain.scala
+++ src/main/scala/it/unibo/sc1819/main/MockScalaMain.scala
@@ -9,16 +9,16 @@ import it.unibo.sc1819.worker.bracket.PhysicLayerMapper
 object MockScalaMain extends App {
 
  val vertxContext = Vertx.vertx
- val racketsConfiguration = List(("192.168.1.155", PhysicLayerMapper(25, 24, 23)))
+ //val racketsConfiguration = List(("192.168.1.155", PhysicLayerMapper(25, 24, 23)))
  val racketList = List("192.168.1.155")
 
 
- val workerVerticle = WorkerVerticle(vertxContext, racketsConfiguration)
+// val workerVerticle = WorkerVerticle(vertxContext, racketsConfiguration)
 
  val serverVerticle = ServerVerticle(vertxContext, racketList, "mockremoteaddress", 8888)
 
  vertxContext.deployVerticle(serverVerticle)
 
- vertxContext.deployVerticle(workerVerticle)
+ //vertxContext.deployVerticle(workerVerticle)
 
 }
