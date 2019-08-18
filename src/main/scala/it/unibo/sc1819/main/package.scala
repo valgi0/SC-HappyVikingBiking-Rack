7b370c814077d2fc77e165d8fdb9b1ff153da480
Tirocigno <fede.naldini1@gmail.com>
2019-08-18 10:32:32 +0200

    Integrated CLI parser i hope

---
+++ src/main/scala/it/unibo/sc1819/main/package.scala
@@ -0,0 +1,20 @@
+package it.unibo.sc1819
+
+/**
+  * Package object containing the main setup
+  */
+package object main {
+
+  val DEFAULT_REMOTE_SERVER_IP = "192.168.1.155"
+  val DEFAULT_REMOTE_SERVER_PORT = 8080
+
+  val DEFAULT_RACK_SERVER_IP = "192.168.1.156"
+  val DEFAULT_RACK_SERVER_PORT = 8888
+
+  val DEFAULT_IP_BRACKET_CONF = "192.168.1.154:6,5,4;"
+
+  val DEFAULT_BRACKETS_SEPARATOR = ";"
+  val DEFAULT_IP_BRACKET_SEP = ":"
+  val DEFAULT_PIN_SEPARATOR = ","
+
+}
